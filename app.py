590b6bfdebf02a20e703b167093e3a1a1d96f61c
Vincent Yang <yf2005@gmail.com>
2022-07-03 23:56:53 +0800

    first version

--- app.py
+++ app.py
@@ -1,23 +1,65 @@
 import pandas as pd
 import streamlit as st
 
-from doc2text import save_uploadedfile
+from doc2text import (
+    docxconvertion,
+    extract_text,
+    get_uploadfiles,
+    remove_uploadfiles,
+    save_uploadedfile,
+)
 
 
 def main():
-    st.subheader("Convert Table to Word")
+    st.subheader("文档转文本")
 
-    uploaded_file_ls = st.file_uploader(
-        "选择新文件上传",
-        type=["docx", "pdf", "doc", "wps"],
-        accept_multiple_files=True,
-        help="选择文件上传",
-    )
+    menuls = ["文档上传", "docx文件格式转换", "文本处理"]
 
-    for uploaded_file in uploaded_file_ls:
-        if uploaded_file is not None:
+    menu = st.sidebar.selectbox("选择菜单", menuls)
 
-            save_uploadedfile(uploaded_file)
+    if menu == "文档上传":
+
+        uploaded_file_ls = st.file_uploader(
+            "选择新文件上传",
+            type=["docx", "pdf", "doc", "wps"],
+            accept_multiple_files=True,
+            help="选择文件上传",
+        )
+
+        for uploaded_file in uploaded_file_ls:
+            if uploaded_file is not None:
+
+                save_uploadedfile(uploaded_file)
+
+    elif menu == "docx文件格式转换":
+        # display uploaded files
+        filels = get_uploadfiles()
+        st.write(filels)
+
+        # button for convert
+        convert_button = st.sidebar.button("docx格式转换")
+        if convert_button:
+            docxconvertion()
+
+        # button for remove files
+        remove_button = st.sidebar.button("删除文件")
+        if remove_button:
+            remove_uploadfiles()
+
+    elif menu == "文本处理":
+        # display uploaded files
+        filels = get_uploadfiles()
+        # convert files to df
+        df = pd.DataFrame({"文件": filels})
+        st.write(df)
+
+        # button for convert
+        convert_button = st.sidebar.button("文件格式转换")
+        if convert_button:
+            dfnew = extract_text(df)
+            st.table(dfnew)
+
+            # st.download_button(dfnew.to_csv(), "文件转换结果.csv")
 
 
 if __name__ == "__main__":
--- doc2text.py
+++ doc2text.py
@@ -1,11 +1,290 @@
+import fnmatch
+import glob
 import os
+import re
+import subprocess
+import zipfile
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
+import docx
+import numpy as np
+import pdfplumber
 import streamlit as st
+from paddleocr import PaddleOCR
+from pdf2image import convert_from_path
 
-uploadfolder = "uploads"
+uploadpath = "uploads/"
+
+ocr = PaddleOCR(use_angle_cls=True, lang="ch")
+
+
+def docxurl2txt(url):
+
+    text = ""
+    try:
+        doc = docx.Document(url)
+        fullText = []
+        for para in doc.paragraphs:
+            fullText.append(para.text)
+            text = "\n".join(fullText)
+    except Exception as e:
+        st.error(str(e))
+
+    return text
+
+
+def pdfurl2txt(url):
+    #     response = requests.get(url)
+    #     source_stream = BytesIO(response.content)
+    result = ""
+    try:
+        #         with pdfplumber.open(source_stream) as pdf:
+        with pdfplumber.open(url) as pdf:
+            for page in pdf.pages:
+                txt = page.extract_text()
+                if txt != "":
+                    result += txt
+    except Exception as e:
+        st.error(str(e))
+    return result
+
+
+def paddleocr2text(image_file):
+    result = ocr.ocr(image_file, cls=True)
+    # print(result)
+    txtls = [line[1][0] for line in result]
+    #     print(txtls)
+    txt = "\n".join(txtls)
+    return txt
+
+
+def pdfurl2ocr(url):
+    PDF_file = Path(url)
+    # Store all the pages of the PDF in a variable
+    image_file_list = []
+    text = ""
+    with TemporaryDirectory() as tempdir:
+        pdf_pages = convert_from_path(PDF_file, 500)
+        # Iterate through all the pages stored above
+        for page_enumeration, page in enumerate(pdf_pages, start=1):
+            # enumerate() "counts" the pages for us.
+
+            # Create a file name to store the image
+            filename = f"{tempdir}\page_{page_enumeration:03}.jpg"
+
+            # Declaring filename for each page of PDF as JPG
+            # For each page, filename will be:
+            # PDF page 1 -> page_001.jpg
+            # PDF page 2 -> page_002.jpg
+            # PDF page 3 -> page_003.jpg
+            # ....
+            # PDF page n -> page_00n.jpg
+
+            # Save the image of the page in system
+            page.save(filename, "JPEG")
+            image_file_list.append(filename)
+
+    # Iterate from 1 to total number of pages
+    for image_file in image_file_list:
+        text += paddleocr2text(image_file)
+    return text
+
+
+def docxurl2ocr(url):
+    z = zipfile.ZipFile(url)
+    all_files = z.namelist()
+    images = sorted(filter(lambda x: x.startswith("word/media/"), all_files))
+
+    # Store all the pages of the PDF in a variable
+    image_file_list = []
+    text = ""
+    with TemporaryDirectory() as tempdir:
+        # Iterate through all the pages stored above
+        for page_enumeration, image in enumerate(images):
+            # enumerate() "counts" the pages for us.
+            img = z.open(image).read()
+            # Create a file name to store the image
+            filename = os.path.basename(image)
+            filepath = os.path.join(tempdir, filename)
+            #             print(filename)
+            # Save the image of the page in system
+            f = open(filepath, "wb")
+            f.write(img)
+            image_file_list.append(filepath)
+
+    # Iterate from 1 to total number of pages
+    for image_file in image_file_list:
+        text += paddleocr2text(image_file)
+    return text
+
+
+def find_files(path: str, glob_pat: str, ignore_case: bool = False):
+    rule = (
+        re.compile(fnmatch.translate(glob_pat), re.IGNORECASE)
+        if ignore_case
+        else re.compile(fnmatch.translate(glob_pat))
+    )
+    return [
+        n for n in glob.glob(os.path.join(path, "*.*"), recursive=True) if rule.match(n)
+    ]
 
 
 def save_uploadedfile(uploadedfile):
-    with open(os.path.join(uploadfolder, uploadedfile.name), "wb") as f:
+    with open(os.path.join(uploadpath, uploadedfile.name), "wb") as f:
         f.write(uploadedfile.getbuffer())
     return st.success("上传文件:{} 成功。".format(uploadedfile.name))
+
+
+def docxconvertion():
+
+    docdest = os.path.join(uploadpath, "doc")
+    wpsdest = os.path.join(uploadpath, "wps")
+    # doccdest = os.path.join(basepath,'docc')
+    docxdest = os.path.join(uploadpath, "docx")
+
+    docfiles = find_files(uploadpath, "*.doc", True)
+    wpsfiles = find_files(uploadpath, "*.wps", True)
+    docxfiles = find_files(uploadpath, "*.docx", True)
+
+    for filepath in docfiles:
+        st.info(filepath)
+        filename = os.path.basename(filepath)
+        #     print(filename)
+        #         output = subprocess.check_output(["soffice","--headless","--convert-to","docx",file,"--outdir",dest])
+        subprocess.call(
+            [
+                "soffice",
+                "--headless",
+                "--convert-to",
+                "docx",
+                filepath,
+                "--outdir",
+                docdest,
+            ]
+        )
+
+    for filepath in wpsfiles:
+        st.info(filepath)
+        filename = os.path.basename(filepath)
+        #     print(filename)
+        #         output = subprocess.check_output(["soffice","--headless","--convert-to","docx",file,"--outdir",dest])
+        subprocess.call(
+            [
+                "soffice",
+                "--headless",
+                "--convert-to",
+                "docx",
+                filepath,
+                "--outdir",
+                wpsdest,
+            ]
+        )
+
+    # for filepath in doccfiles:
+    #     print (filepath)
+    #     filename=os.path.basename(filepath)
+    # #     print(filename)
+    # #         output = subprocess.check_output(["soffice","--headless","--convert-to","docx",file,"--outdir",dest])
+    #     subprocess.call(['soffice', '--headless', '--convert-to', 'docx', filepath,"--outdir",doccdest])
+
+    for filepath in docxfiles:
+        st.info(filepath)
+        filename = os.path.basename(filepath)
+        #     print(filename)
+        #         output = subprocess.check_output(["soffice","--headless","--convert-to","docx",file,"--outdir",dest])
+        subprocess.call(
+            [
+                "soffice",
+                "--headless",
+                "--convert-to",
+                "docx",
+                filepath,
+                "--outdir",
+                docxdest,
+            ]
+        )
+
+
+def get_uploadfiles():
+    fileslist = glob.glob(uploadpath + "/*.*", recursive=True)
+    basenamels = []
+    for file in fileslist:
+        basenamels.append(os.path.basename(file))
+    return basenamels
+
+
+def remove_uploadfiles():
+    files = glob.glob(uploadpath + "/*.*", recursive=True)
+
+    for f in files:
+        try:
+            os.remove(f)
+        except OSError as e:
+            st.error("Error: %s : %s" % (f, e.strerror))
+
+
+# convert all files in uploadfolder to text
+def convert_uploadfiles(txtls):
+
+    resls = []
+    for file in txtls:
+        st.info(file)
+        try:
+            #     datapath=filepath+file
+            datapath = os.path.join(uploadpath, file)
+            #     get file ext
+            base, ext = os.path.splitext(file)
+
+            if ext.lower() == ".doc":
+                datapath = uploadpath + "doc/" + base + ".docx"
+                st.info(datapath)
+                text = docxurl2txt(datapath)
+                text1 = text.translate(str.maketrans("", "", r" \n\t\r\s"))
+                if text1 == "":
+                    text = docxurl2ocr(datapath)
+
+            elif ext.lower() == ".wps":
+                datapath = uploadpath + "wps/" + base + ".docx"
+                st.info(datapath)
+                text = docxurl2txt(datapath)
+                text1 = text.translate(str.maketrans("", "", r" \n\t\r\s"))
+                if text1 == "":
+                    text = docxurl2ocr(datapath)
+
+            #         elif ext.lower()=='doc.docx':
+            #             datapath=os.path.join(filepath,'docc',file)
+            #             print(datapath)
+            #             text=docxurl2txt(datapath)
+            elif ext.lower() == ".docx":
+                st.info(datapath)
+                text = docxurl2txt(datapath)
+                text1 = text.translate(str.maketrans("", "", r" \n\t\r\s"))
+                if text1 == "":
+                    datapath = os.path.join(uploadpath, "docx", file)
+                    st.info(datapath)
+                    text = docxurl2txt(datapath)
+                    text2 = text.translate(str.maketrans("", "", r" \n\t\r\s"))
+                    if text2 == "":
+                        text = docxurl2ocr(datapath)
+
+            #         elif ext.lower()=='.pdf':
+            #             text=pdfurl2txt(datapath)
+            #             text=text.translate(str.maketrans('', '', ' \n\t\r\s'))
+            #             if text=='':
+            #                 text=pdfurl2ocr(datapath)
+            else:
+                text = np.nan
+        except Exception as e:
+            st.error(str(e))
+            text = ""
+        resls.append(text)
+    return resls
+
+
+# extract text from files
+def extract_text(df):
+    txtls = df["文件"].tolist()
+    resls = convert_uploadfiles(txtls)
+    df["文本"] = resls
+    return df
